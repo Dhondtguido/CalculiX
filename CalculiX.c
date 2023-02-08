97b569fc6403337d059e8f31014b3fbbc25bbde7
Rafal <rafal.brzegowy@yahoo.com>
2023-02-08 08:22:24 +0100

    add info about double precision version (i8)

--- CalculiX.c
+++ CalculiX.c
@@ -149,7 +149,11 @@ int main(int argc,char *argv[])
   FORTRAN(openfile,(jobnamef));
 
   printf("\n************************************************************\n\n");
+#ifdef INTSIZE64
+  printf("CalculiX Version DEVELOPMENT i8, Copyright(C) 1998-2015 Guido Dhondt\n");
+#else
   printf("CalculiX Version DEVELOPMENT, Copyright(C) 1998-2015 Guido Dhondt\n");
+#endif
   printf("CalculiX comes with ABSOLUTELY NO WARRANTY. This is free\n");
   printf("software, and you are welcome to redistribute it under\n");
   printf("certain conditions, see gpl.htm\n\n");
