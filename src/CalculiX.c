663785d1b8febf9a09079fc699cef8c0677703c4
Guido Dhondt <dhondt@t-online.de>
2023-04-03 21:50:08 +0200

    "worked on normalsonsurface_se.f"

--- src/CalculiX.c
+++ src/CalculiX.c
@@ -154,7 +154,7 @@ int main(int argc,char *argv[])
   printf("software, and you are welcome to redistribute it under\n");
   printf("certain conditions, see gpl.htm\n\n");
   printf("************************************************************\n\n");
-  printf("You are using an executable made on Sat Apr  1 18:25:27 CEST 2023\n");
+  printf("You are using an executable made on Mon Apr  3 21:49:35 CEST 2023\n");
   fflush(stdout);
 
   NNEW(ipoinp,ITG,2*nentries);
