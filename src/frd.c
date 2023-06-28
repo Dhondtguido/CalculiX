490aa40e00bf26c283edb73965603fb019801f5a
wr1 <8971152+wr1@users.noreply.github.com>
2023-06-28 13:26:07 +0200

    add makefile for ubuntu

--- src/frd.c
+++ src/frd.c
@@ -200,7 +200,7 @@ void frd(double *co,ITG *nk,ITG *kon,ITG *ipkon,char *lakon,ITG *ne0,
     fprintf(f1,"%5sUHOST                                                              \n",p1);
     fprintf(f1,"%5sUPGM               CalculiX                                        \n",p1);
     fprintf(f1,"%5sUVERSION           Version DEVELOPMENT                             \n",p1);
-    fprintf(f1,"%5sUCOMPILETIME       Tue Jun 27 22:28:11 CEST 2023                    \n",p1);
+    fprintf(f1,"%5sUCOMPILETIME       Wed Jun 28 01:25:38 PM CEST 2023                    \n",p1);
     fprintf(f1,"%5sUDIR                                                               \n",p1);
     fprintf(f1,"%5sUDBN                                                               \n",p1);
     
