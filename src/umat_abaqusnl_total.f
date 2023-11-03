19656d2156a667353be2c664e250b6508dc81f87
Guido Dhondt <dhondt@t-online.de>
2023-11-03 18:38:57 +0100

    "worked on umat_abaqusnl_total.f and umat_abaqusnl.f (consistent tangent)"

--- src/umat_abaqusnl_total.f
+++ src/umat_abaqusnl_total.f
@@ -45,6 +45,13 @@
 !     xkl is the total deformation gradient (including thermal effects)
 !     vj is the total Jacobian (including thermal effects)
 ! 
+!     Reference for the derivative of a function of C w.r.t C (C is
+!     the Cauchy-Green tensor):
+! 
+!     Dhondt, G., Hackenberg, H.-P., Use of a rotation-invariant linear
+!     strain measure for linear elastic crack propagation calculations,
+!     Eng. Fract. Mech. 247(2021) 107634.
+! 
       implicit none
 !     
       character*80 amat,amatloc
@@ -66,7 +73,8 @@
      &     c4(21),d4(21),dxm1dc(21),dxm2dc(21),dxm3dc(21),
      &     dum1dc(21),delndc(21),dum1dcexp(3,3,3,3),delndcexp(3,3,3,3),
      &     stiffexp(3,3,3,3),term1(36),term24(36),term3(36),
-     &     stiffasym(36),aa(21),bb(21),dwum1(3),dweln(3)
+     &     stiffasym(36),aa(21),bb(21),dwum1(3),dweln(3),ym14(21),
+     &     ym24(21),ym34(21)
 !     
       kal=reshape((/1,1,2,2,3,3,1,2,1,3,2,3/),(/2,6/))
 !     
@@ -388,29 +396,29 @@ c      write(*,*) 'umat_abaqusnl_total ',(emec(i),i=1,6)
 !     calculate the stiffness matrix (the matrix is symmetrized)
 !     
       if(icmd.ne.3) then
-!     
-!     rotating the stiffness coefficients into the global system
-!     
-        call anisotropic(stiff,ya)
-!     
-        do jj=1,21
-          j1=kel(1,jj)
-          j2=kel(2,jj)
-          j3=kel(3,jj)
-          j4=kel(4,jj)
-          stiff(jj)=0.d0
-          do j5=1,3
-            do j6=1,3
-              do j7=1,3
-                do j8=1,3
-                  stiff(jj)=stiff(jj)+ya(j5,j6,j7,j8)*
-     &                 tkl(j1,j5)*tkl(j2,j6)*tkl(j3,j7)*tkl(j4,j8)
-                enddo
-              enddo
-            enddo
-          enddo
-          stiff(jj)=stiff(jj)*vj/expansion**2
-        enddo
+c!     
+c!     rotating the stiffness coefficients into the global system
+c!     
+c        call anisotropic(stiff,ya)
+c!     
+c        do jj=1,21
+c          j1=kel(1,jj)
+c          j2=kel(2,jj)
+c          j3=kel(3,jj)
+c          j4=kel(4,jj)
+c          stiff(jj)=0.d0
+c          do j5=1,3
+c            do j6=1,3
+c              do j7=1,3
+c                do j8=1,3
+c                  stiff(jj)=stiff(jj)+ya(j5,j6,j7,j8)*
+c     &                 tkl(j1,j5)*tkl(j2,j6)*tkl(j3,j7)*tkl(j4,j8)
+c                enddo
+c              enddo
+c            enddo
+c          enddo
+c          stiff(jj)=stiff(jj)*vj/expansion**2
+c        enddo
 !     
 !       sccum1=scc*um1
 !
@@ -477,30 +485,6 @@ c      write(*,*) 'umat_abaqusnl_total ',(emec(i),i=1,6)
         ym3(3,1)=xm3(5)
         ym3(3,2)=xm3(6)
         ym3(3,3)=xm3(3)
-!     
-!       derivative of c*c w.r.t. c (symmetric 4th order tensor)     
-!     
-        c4(1)=2.d0*c(1,1)
-        c4(2)=0.d0
-        c4(3)=2.d0*c(2,2)
-        c4(4)=0.d0
-        c4(5)=0.d0
-        c4(6)=2.d0*c(3,3)
-        c4(7)=c(2,1)
-        c4(8)=c(1,2)
-        c4(9)=0.d0
-        c4(10)=(c(2,2)+c(1,1))/2.d0
-        c4(11)=c(3,1)
-        c4(12)=0.d0
-        c4(13)=c(1,3)
-        c4(14)=c(3,2)/2.d0
-        c4(15)=(c(3,3)+c(1,1))/2.d0
-        c4(16)=0.d0
-        c4(17)=c(3,2)
-        c4(18)=c(2,3)
-        c4(19)=c(3,1)/2.d0
-        c4(20)=c(2,1)/2.d0
-        c4(21)=(c(3,3)+c(2,2))/2.d0
 !
 !       calculating the derivative of the eigenvalue functions
 !
@@ -512,40 +496,192 @@ c      write(*,*) 'umat_abaqusnl_total ',(emec(i),i=1,6)
 !       calculating A, B, dM_i/dC, dU^{-1}/dC and dln(e)/dC
 !       (all of them symmetric, i.e. 21 constants)
 !
+          write(*,*) 'eigenvalues ',w(1),w(2),w(3)
         if((dabs(w(1)-w(2)).lt.1.d-10).and.
-     &       (dabs(w(2)-w(2)).lt.1.d-10)) then
+     &       (dabs(w(2)-w(3)).lt.1.d-10)) then
 !
 !         three equal eigenvalues
-!
-          do jj=1,21
+!     
+        do jj=1,21
             dum1dc(jj)=d4(jj)*dwum1(1)
             delndc(jj)=d4(jj)*dweln(1)
           enddo
-        else
-          write(*,*) 'eigenvalues ',w(1),w(2),w(3)
+        elseif(dabs(w(1)-w(2)).lt.1.d-10) then
+          write(*,*) 'entering the new branch'
+!
+!         first and second eigenvalue are equal    
+!         (M3 o I + I o M3)'    
+!         note: o is the dyadic product     
+!     
+          c4(1)=2.d0*ym3(1,1)
+          c4(2)=0.d0
+          c4(3)=2.d0*ym3(2,2)
+          c4(4)=0.d0
+          c4(5)=0.d0
+          c4(6)=2.d0*ym3(3,3)
+          c4(7)=ym3(2,1)
+          c4(8)=ym3(1,2)
+          c4(9)=0.d0
+          c4(10)=(ym3(2,2)+ym3(1,1))/2.d0
+          c4(11)=ym3(3,1)
+          c4(12)=0.d0
+          c4(13)=ym3(1,3)
+          c4(14)=ym3(3,2)/2.d0
+          c4(15)=(ym3(3,3)+ym3(1,1))/2.d0
+          c4(16)=0.d0
+          c4(17)=ym3(3,2)
+          c4(18)=ym3(2,3)
+          c4(19)=ym3(3,1)/2.d0
+          c4(20)=ym3(2,1)/2.d0
+          c4(21)=(ym3(3,3)+ym3(2,2))/2.d0
+!
           do jj=1,21
             j1=kel(1,jj)
             j2=kel(2,jj)
             j3=kel(3,jj)
             j4=kel(4,jj)
-            aa(jj)=d4(jj)-ym1(j1,j2)*ym1(j3,j4)
-     &           -ym2(j1,j2)*ym2(j3,j4)
-     &           -ym3(j1,j2)*ym3(j3,j4)
-            bb(jj)=c4(jj)-2.d0*(w(1)*ym1(j1,j2)*ym1(j3,j4)
-     &           +w(2)*ym2(j1,j2)*ym2(j3,j4)
-     &           +w(3)*ym3(j1,j2)*ym3(j3,j4))
+            ym34(jj)=ym3(j1,j2)*ym3(j3,j4)
+            dum1dc(jj)=dwum1(3)*ym34(jj)+
+     &           (wum1(3)-wum1(1))/(w(3)-w(1))*(c4(jj)-2.d0*ym34(jj))+
+     &           dwum1(1)*(d4(jj)+ym34(jj)-c4(jj))
+            delndc(jj)=dweln(3)*ym34(jj)+
+     &           (weln(3)-weln(1))/(w(3)-w(1))*(c4(jj)-2.d0*ym34(jj))+
+     &           dweln(1)*(d4(jj)+ym34(jj)-c4(jj))
+          enddo
+         elseif(dabs(w(2)-w(3)).lt.1.d-10) then
+c          write(*,*) 'entering the new branch'
+!
+!         second and third eigenvalue are equal    
+!         (M1 o I + I o M1)'    
+!         note: o is the dyadic product     
+!     
+          c4(1)=2.d0*ym1(1,1)
+          c4(2)=0.d0
+          c4(3)=2.d0*ym1(2,2)
+          c4(4)=0.d0
+          c4(5)=0.d0
+          c4(6)=2.d0*ym1(3,3)
+          c4(7)=ym1(2,1)
+          c4(8)=ym1(1,2)
+          c4(9)=0.d0
+          c4(10)=(ym1(2,2)+ym1(1,1))/2.d0
+          c4(11)=ym1(3,1)
+          c4(12)=0.d0
+          c4(13)=ym1(1,3)
+          c4(14)=ym1(3,2)/2.d0
+          c4(15)=(ym1(3,3)+ym1(1,1))/2.d0
+          c4(16)=0.d0
+          c4(17)=ym1(3,2)
+          c4(18)=ym1(2,3)
+          c4(19)=ym1(3,1)/2.d0
+          c4(20)=ym1(2,1)/2.d0
+          c4(21)=(ym1(3,3)+ym1(2,2))/2.d0
+!
+          do jj=1,21
+            j1=kel(1,jj)
+            j2=kel(2,jj)
+            j3=kel(3,jj)
+            j4=kel(4,jj)
+            ym14(jj)=ym1(j1,j2)*ym1(j3,j4)
+            dum1dc(jj)=dwum1(1)*ym14(jj)+
+     &           (wum1(1)-wum1(2))/(w(1)-w(2))*(c4(jj)-2.d0*ym14(jj))+
+     &           dwum1(2)*(d4(jj)+ym14(jj)-c4(jj))
+            delndc(jj)=dweln(1)*ym14(jj)+
+     &           (weln(1)-weln(2))/(w(1)-w(2))*(c4(jj)-2.d0*ym14(jj))+
+     &           dweln(2)*(d4(jj)+ym14(jj)-c4(jj))
+          enddo
+        elseif(dabs(w(3)-w(1)).lt.1.d-10) then
+c          write(*,*) 'entering the new branch'
+!
+!         third and first eigenvalue are equal    
+!         (M2 o I + I o M2)'    
+!         note: o is the dyadic product     
+!     
+          c4(1)=2.d0*ym2(1,1)
+          c4(2)=0.d0
+          c4(3)=2.d0*ym2(2,2)
+          c4(4)=0.d0
+          c4(5)=0.d0
+          c4(6)=2.d0*ym2(3,3)
+          c4(7)=ym2(2,1)
+          c4(8)=ym2(1,2)
+          c4(9)=0.d0
+          c4(10)=(ym2(2,2)+ym2(1,1))/2.d0
+          c4(11)=ym2(3,1)
+          c4(12)=0.d0
+          c4(13)=ym2(1,3)
+          c4(14)=ym2(3,2)/2.d0
+          c4(15)=(ym2(3,3)+ym2(1,1))/2.d0
+          c4(16)=0.d0
+          c4(17)=ym2(3,2)
+          c4(18)=ym2(2,3)
+          c4(19)=ym2(3,1)/2.d0
+          c4(20)=ym2(2,1)/2.d0
+          c4(21)=(ym2(3,3)+ym2(2,2))/2.d0
+!
+          do jj=1,21
+            j1=kel(1,jj)
+            j2=kel(2,jj)
+            j3=kel(3,jj)
+            j4=kel(4,jj)
+            ym24(jj)=ym2(j1,j2)*ym2(j3,j4)
+            dum1dc(jj)=dwum1(2)*ym24(jj)+
+     &           (wum1(2)-wum1(3))/(w(2)-w(3))*(c4(jj)-2.d0*ym24(jj))+
+     &           dwum1(3)*(d4(jj)+ym24(jj)-c4(jj))
+            delndc(jj)=dweln(2)*ym24(jj)+
+     &           (weln(2)-weln(3))/(w(2)-w(3))*(c4(jj)-2.d0*ym24(jj))+
+     &           dweln(3)*(d4(jj)+ym24(jj)-c4(jj))
+          enddo
+       else
+!
+!         derivative of c*c w.r.t. c (symmetric 4th order tensor)     
+!     
+          c4(1)=2.d0*c(1,1)
+          c4(2)=0.d0
+          c4(3)=2.d0*c(2,2)
+          c4(4)=0.d0
+          c4(5)=0.d0
+          c4(6)=2.d0*c(3,3)
+          c4(7)=c(2,1)
+          c4(8)=c(1,2)
+          c4(9)=0.d0
+          c4(10)=(c(2,2)+c(1,1))/2.d0
+          c4(11)=c(3,1)
+          c4(12)=0.d0
+          c4(13)=c(1,3)
+          c4(14)=c(3,2)/2.d0
+          c4(15)=(c(3,3)+c(1,1))/2.d0
+          c4(16)=0.d0
+          c4(17)=c(3,2)
+          c4(18)=c(2,3)
+          c4(19)=c(3,1)/2.d0
+          c4(20)=c(2,1)/2.d0
+          c4(21)=(c(3,3)+c(2,2))/2.d0
+!
+          do jj=1,21
+            j1=kel(1,jj)
+            j2=kel(2,jj)
+            j3=kel(3,jj)
+            j4=kel(4,jj)
+            ym14(jj)=ym1(j1,j2)*ym1(j3,j4)
+            ym24(jj)=ym2(j1,j2)*ym2(j3,j4)
+            ym34(jj)=ym3(j1,j2)*ym3(j3,j4)
+            aa(jj)=d4(jj)-ym14(jj)-ym24(jj)-ym34(jj)
+            bb(jj)=c4(jj)-2.d0*(w(1)*ym14(jj)
+     &           +w(2)*ym24(jj)
+     &           +w(3)*ym34(jj))
             dxm1dc(jj)=(bb(jj)-(w(2)+w(3))*aa(jj))/
      &           ((w(1)-w(2))*(w(1)-w(3)))
             dxm2dc(jj)=(bb(jj)-(w(1)+w(3))*aa(jj))/
      &           ((w(2)-w(1))*(w(2)-w(3)))
             dxm3dc(jj)=(bb(jj)-(w(1)+w(2))*aa(jj))/
      &           ((w(3)-w(1))*(w(3)-w(2)))
-            dum1dc(jj)=ym1(j1,j2)*ym1(j3,j4)*dwum1(1)+wum1(1)*dxm1dc(jj)
-     &           +ym2(j1,j2)*ym2(j3,j4)*dwum1(2)+wum1(2)*dxm2dc(jj)
-     &           +ym3(j1,j2)*ym3(j3,j4)*dwum1(3)+wum1(3)*dxm3dc(jj)
-            delndc(jj)=ym1(j1,j2)*ym1(j3,j4)*dweln(1)+weln(1)*dxm1dc(jj)
-     &           +ym2(j1,j2)*ym2(j3,j4)*dweln(2)+weln(2)*dxm2dc(jj)
-     &           +ym3(j1,j2)*ym3(j3,j4)*dweln(3)+weln(3)*dxm3dc(jj)
+            dum1dc(jj)=ym14(jj)*dwum1(1)+wum1(1)*dxm1dc(jj)
+     &           +ym24(jj)*dwum1(2)+wum1(2)*dxm2dc(jj)
+     &           +ym34(jj)*dwum1(3)+wum1(3)*dxm3dc(jj)
+            delndc(jj)=ym14(jj)*dweln(1)+weln(1)*dxm1dc(jj)
+     &           +ym24(jj)*dweln(2)+weln(2)*dxm2dc(jj)
+     &           +ym34(jj)*dweln(3)+weln(3)*dxm3dc(jj)
           enddo
         endif
 !     
@@ -584,54 +720,41 @@ c      write(*,*) 'umat_abaqusnl_total ',(emec(i),i=1,6)
            enddo
            stiffasym(jj)=vj*(term1(jj)+2.d0*(term24(jj)+term3(jj)))/
      &          expansion**2
-c           stiffasym(jj)=vj*(term1(jj)+2.d0*(term3(jj)))/
-c     &          expansion**2
          enddo
 !
 !        symmetrizing stiffasym -> stiff
 !
-c         stiff(1)=stiffasym(1)
-c         stiff(2)=(stiffasym(2)+stiffasym(22))/2.d0
-c         stiff(3)=stiffasym(3)
-c         stiff(4)=(stiffasym(4)+stiffasym(23))/2.d0
-c         stiff(5)=(stiffasym(5)+stiffasym(24))/2.d0
-c         stiff(6)=stiffasym(6)
-c         stiff(7)=(stiffasym(7)+stiffasym(25))/2.d0
-c         stiff(8)=(stiffasym(8)+stiffasym(26))/2.d0
-c         stiff(9)=(stiffasym(9)+stiffasym(27))/2.d0
-c         stiff(10)=stiffasym(10)
-c         stiff(11)=(stiffasym(11)+stiffasym(28))/2.d0
-c         stiff(12)=(stiffasym(12)+stiffasym(29))/2.d0
-c         stiff(13)=(stiffasym(13)+stiffasym(30))/2.d0
-c         stiff(14)=(stiffasym(14)+stiffasym(31))/2.d0
-c         stiff(15)=stiffasym(15)
-c         stiff(16)=(stiffasym(16)+stiffasym(32))/2.d0
-c         stiff(17)=(stiffasym(17)+stiffasym(33))/2.d0
-c         stiff(18)=(stiffasym(18)+stiffasym(34))/2.d0
-c         stiff(19)=(stiffasym(19)+stiffasym(35))/2.d0
-c         stiff(20)=(stiffasym(20)+stiffasym(36))/2.d0
-c         stiff(21)=stiffasym(21)
+         stiff(1)=stiffasym(1)
+         stiff(2)=(stiffasym(2)+stiffasym(22))/2.d0
+         stiff(3)=stiffasym(3)
+         stiff(4)=(stiffasym(4)+stiffasym(23))/2.d0
+         stiff(5)=(stiffasym(5)+stiffasym(24))/2.d0
+         stiff(6)=stiffasym(6)
+         stiff(7)=(stiffasym(7)+stiffasym(25))/2.d0
+         stiff(8)=(stiffasym(8)+stiffasym(26))/2.d0
+         stiff(9)=(stiffasym(9)+stiffasym(27))/2.d0
+         stiff(10)=stiffasym(10)
+         stiff(11)=(stiffasym(11)+stiffasym(28))/2.d0
+         stiff(12)=(stiffasym(12)+stiffasym(29))/2.d0
+         stiff(13)=(stiffasym(13)+stiffasym(30))/2.d0
+         stiff(14)=(stiffasym(14)+stiffasym(31))/2.d0
+         stiff(15)=stiffasym(15)
+         stiff(16)=(stiffasym(16)+stiffasym(32))/2.d0
+         stiff(17)=(stiffasym(17)+stiffasym(33))/2.d0
+         stiff(18)=(stiffasym(18)+stiffasym(34))/2.d0
+         stiff(19)=(stiffasym(19)+stiffasym(35))/2.d0
+         stiff(20)=(stiffasym(20)+stiffasym(36))/2.d0
+         stiff(21)=stiffasym(21)
 c
-         write(*,*) 'umat_abaqusnl_total'
-         do i=1,21
-c     write(*,*) i,stiff(i),stiffasym(i)
-           write(*,'(i5,5(1x,e11.4))') i,stiffasym(i),stiff(i),
-     &          vj*term1(i)/expansion**2,
-     &          vj*2.d0*term24(i)/expansion**2,
-     &          vj*2.d0*term3(i)/expansion**2
-         enddo
-!         
-c         do jj=1,21
-c          k=kel(1,jj)
-c          l=kel(2,jj)
-c          p=kel(3,jj)
-c          q=kel(4,jj)
-c          write(*,100)jj,d2(p,k),q,l,d2(q,k),p,l,d2(q,l),p,k,d2(p,l),q,k
-c        enddo
-c 100    format('c4(',i2,')=(',f2.0,'*c(',i1,',',i1,')+',f2.0,
-c     &       '*c(',i1,',',i1,')+',
-c     &       f2.0,'*c(',i1,',',i1,')+',f2.0,'*c(',i1,',',i1,'))/2.d0')
-c        stop
+c         write(*,*) 'umat_abaqusnl_total'
+c         do i=1,21
+cc     write(*,*) i,stiff(i),stiffasym(i)
+c           write(*,'(i5,5(1x,e11.4))') i,stiff(i),
+c     &          dabs(stiff(i)-stiffasym(i))/dabs(stiff(i)),
+c     &          vj*term1(i)/expansion**2,
+c     &          vj*2.d0*term24(i)/expansion**2,
+c     &          vj*2.d0*term3(i)/expansion**2
+c         enddo
 !     
        endif
 !     
