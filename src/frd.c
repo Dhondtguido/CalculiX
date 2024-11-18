ed7c5879e8fea26925f69eb28eb4ed444efc19b7
Guido Dhondt <dhondt@t-online.de>
2024-11-18 22:01:26 +0100

    ""

--- src/frd.c
+++ src/frd.c
@@ -200,7 +200,7 @@ void frd(double *co,ITG *nk,ITG *kon,ITG *ipkon,char *lakon,ITG *ne0,
     fprintf(f1,"%5sUHOST                                                              \n",p1);
     fprintf(f1,"%5sUPGM               CalculiX                                        \n",p1);
     fprintf(f1,"%5sUVERSION           Version DEVELOPMENT                             \n",p1);
-    fprintf(f1,"%5sUCOMPILETIME       Fri Nov 15 14:17:22 CET 2024                    \n",p1);
+    fprintf(f1,"%5sUCOMPILETIME       Mon Nov 18 22:00:56 CET 2024                    \n",p1);
     fprintf(f1,"%5sUDIR                                                               \n",p1);
     fprintf(f1,"%5sUDBN                                                               \n",p1);
     
