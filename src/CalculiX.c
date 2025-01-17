8f96bce40f50436c0255d556b3e9ebadaae55067
Guido Dhondt <dhondt@t-online.de>
2025-01-17 13:38:46 +0100

    "multiplicative decomposition: test examples run"

--- src/CalculiX.c
+++ src/CalculiX.c
@@ -158,7 +158,7 @@ int main(int argc,char *argv[])
   printf("software, and you are welcome to redistribute it under\n");
   printf("certain conditions, see gpl.htm\n\n");
   printf("************************************************************\n\n");
-  printf("You are using an executable made on Tue Jan 14 19:00:46 CET 2025\n");
+  printf("You are using an executable made on Wed Jan 15 11:10:14 CET 2025\n");
   fflush(stdout);
 
   NNEW(ipoinp,ITG,2*nentries);
--- src/frd.c
+++ src/frd.c
@@ -200,7 +200,7 @@ void frd(double *co,ITG *nk,ITG *kon,ITG *ipkon,char *lakon,ITG *ne0,
     fprintf(f1,"%5sUHOST                                                              \n",p1);
     fprintf(f1,"%5sUPGM               CalculiX                                        \n",p1);
     fprintf(f1,"%5sUVERSION           Version DEVELOPMENT                             \n",p1);
-    fprintf(f1,"%5sUCOMPILETIME       Tue Jan 14 19:00:46 CET 2025                    \n",p1);
+    fprintf(f1,"%5sUCOMPILETIME       Wed Jan 15 11:10:14 CET 2025                    \n",p1);
     fprintf(f1,"%5sUDIR                                                               \n",p1);
     fprintf(f1,"%5sUDBN                                                               \n",p1);
     
