b43a1d6ece80d540013225dbd0bb59ca03d95a54
Guido Dhondt <dhondt@t-online.de>
2025-05-16 12:13:44 +0200

    "corrected small errors in remlagrangemult.f and printoutcontact.f"

--- src/CalculiXstep.c
+++ src/CalculiXstep.c
@@ -301,7 +301,7 @@ void CalculiXstep(int argc,char argv[][133],ITG **nelemloadp,double **xloadp,
     printf("software, and you are welcome to redistribute it under\n");
     printf("certain conditions, see gpl.htm\n\n");
     printf("************************************************************\n\n");
-    printf("You are using an executable made on Fri May  9 13:59:36 CEST 2025\n");
+    printf("You are using an executable made on Fri May 16 12:10:42 CEST 2025\n");
     fflush(stdout);
 
     NNEW(ipoinp,ITG,2*nentries);
