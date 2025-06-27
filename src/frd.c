a347622362637c6232afe471586f36e67c421ffc
Guido Dhondt <dhondt@t-online.de>
2025-06-27 18:13:37 +0200

    "removed errors in readnewmesh.c and updated basis.f"

--- src/frd.c
+++ src/frd.c
@@ -202,7 +202,7 @@ void frd(double *co,ITG *nk,ITG *kon,ITG *ipkon,char *lakon,ITG *ne0,
     fprintf(f1,"%5sUHOST                                                              \n",p1);
     fprintf(f1,"%5sUPGM               CalculiX                                        \n",p1);
     fprintf(f1,"%5sUVERSION           Version DEVELOPMENT                             \n",p1);
-    fprintf(f1,"%5sUCOMPILETIME       Fri Jun 27 15:42:07 CEST 2025                    \n",p1);
+    fprintf(f1,"%5sUCOMPILETIME       Fri Jun 27 18:10:13 CEST 2025                    \n",p1);
     fprintf(f1,"%5sUDIR                                                               \n",p1);
     fprintf(f1,"%5sUDBN                                                               \n",p1);
     
--- src/readnewmesh.c
+++ src/readnewmesh.c
@@ -64,7 +64,7 @@ void readnewmesh(char *jobnamec,ITG *nboun,ITG *nodeboun,ITG *iamboun,
     masterurffile[132]="";
 
   ITG *integerglob=NULL,iglob=1,irefine=1,*inodestet=NULL,nnodestet=0,i,
-    istart,j,iquadratic,nenew,nline,nset_=0,*ipoinp=NULL,nkold,
+    istart,j,iquadratic,nenew,nline,nset_=0,*ipoinp=NULL,nkold,nkmt,
     *inp=NULL,*ipoinpc=NULL,idummy[2]={0,0},nuel_=0,inp_size,nentries=19,
     nkold_,neold_,nkonold_,*ielprop=NULL,*iponor=NULL,*iponoel=NULL,
     *rig=NULL,*ne2boun=NULL,*ielorien=NULL,*inotr=NULL,im,mt=mi[1]+1,
@@ -73,15 +73,13 @@ void readnewmesh(char *jobnamec,ITG *nboun,ITG *nodeboun,ITG *iamboun,
     limit,*ipobody2=NULL,ifreebody2,index,index2,*iprfn=NULL,*konrfn=NULL,
     *jq=NULL,*irow=NULL,*icol=NULL,*loc=NULL,*irowt=NULL,*jqt=NULL,
     *itemp=NULL,*ixcol=NULL,*ipoface=NULL,*nodface=NULL,iamplitude,
-    num_cpus,isize,idelta,isum,num_cpus_loc,jstart,kstart,k,m,jstartn,
+    num_cpus,idelta,isum,num_cpus_loc,jstart,kstart,k,m,jstartn,
     kstartn;
-
-  static ITG nkn1,nkn2;
 
   double *doubleglob=NULL,sigma=0.,*thickn=NULL,*thicke=NULL,*offset=NULL,
     *t0=NULL,*t0g=NULL,*t1g=NULL,*prestr=NULL,*vold=NULL,*veold=NULL,
     *randomval=NULL,*t1=NULL,*coefmpc=NULL,*co=NULL,*ratiorfn=NULL,
-    *au=NULL;
+    *au=NULL,*t0new=NULL,*t1new=NULL,*vnew=NULL,*venew=NULL;
       
   /* variables for multithreading procedure */
   
@@ -261,22 +259,9 @@ void readnewmesh(char *jobnamec,ITG *nboun,ITG *nodeboun,ITG *iamboun,
        the new nodes within the old mesh */
     
     pthread_t tid[num_cpus];
-
-    //  if(*nk==nkold){
-
-      /* smoothing only */
-      
-      nkn1=0;nkn2=*nk;
-      //    }else{
-
-      /* mesh refinement */
-      
-      //     nkn1=nkold;nkn2=*nk;
-      // }
     
-    isize=nkn2-nkn1;
-    if(num_cpus>isize){
-      num_cpus_loc=isize;
+    if(num_cpus>*nk){
+      num_cpus_loc=*nk;
     }else{
       num_cpus_loc=num_cpus;
     }
@@ -290,16 +275,16 @@ void readnewmesh(char *jobnamec,ITG *nboun,ITG *nodeboun,ITG *iamboun,
 
     /* dividing the new nodes among the cpus */
     
-    idelta=(ITG)floor(isize)/(double)(num_cpus_loc);
+    idelta=(ITG)floor(*nk)/(double)(num_cpus_loc);
     ideltamax=idelta;
-    isum=nkn1;
+    isum=0;
     for(i=0;i<num_cpus_loc;i++){
       nkapar[i]=isum;
       if(i!=num_cpus_loc-1){
 	isum+=idelta;
       }else{
-	isum=nkn1+isize;
-	if(ideltamax<nkn1+isize-nkapar[i]) ideltamax=nkn1+isize-nkapar[i];
+	isum=*nk;
+	if(ideltamax<*nk-nkapar[i]) ideltamax=*nk-nkapar[i];
       }
       nkbpar[i]=isum;
     }
@@ -326,12 +311,12 @@ void readnewmesh(char *jobnamec,ITG *nboun,ITG *nodeboun,ITG *iamboun,
 
     for(i=0;i<num_cpus_loc;i++){
       jstart=i*(ideltamax+1);
-      jstartn=nkapar[i]-nkn1;
+      jstartn=nkapar[i];
       kstart=i*20*ideltamax;
       if(i==0){
 	kstartn=0;
       }else{
-	kstartn=iprfn[nkbpar[i-1]-nkn1];
+	kstartn=iprfn[nkbpar[i-1]];
       }
       for(k=0;k<nkbpar[i]-nkapar[i];k++){
 	iprfn[jstartn+k]=kstartn+iprfn[jstart+k];
@@ -343,21 +328,23 @@ void readnewmesh(char *jobnamec,ITG *nboun,ITG *nodeboun,ITG *iamboun,
       iprfn[jstartn+nkbpar[i]-nkapar[i]]=kstartn+iprfn[jstart+nkbpar[i]-nkapar[i]];
     }
 
-    RENEW(iprfn,ITG,nkn2-nkn1+1);
-    RENEW(konrfn,ITG,iprfn[nkn2-nkn1]);
-    RENEW(ratiorfn,double,iprfn[nkn2-nkn1]);
+    RENEW(iprfn,ITG,*nk+1);
+    RENEW(konrfn,ITG,iprfn[*nk]);
+    RENEW(ratiorfn,double,iprfn[*nk]);
 
     /* interpolating the initial temperatures t0 */
 
     if(ithermal[0]>0){
-      for(i=0;i<nkn2-nkn1;i++){
-	t0[nkn1+i]=0.;
+      NNEW(t0new,double,*nk);
+      for(i=0;i<*nk;i++){
 	for(j=0;j<iprfn[i+1]-iprfn[i];j++){
-	  t0[nkn1+i]+=ratiorfn[iprfn[i]+j]*t0[konrfn[iprfn[i]+j]-1];
+	  t0new[i]+=ratiorfn[iprfn[i]+j]*t0[konrfn[iprfn[i]+j]-1];
 	}
       }
+      cpypardou(t0,t0new,nk,&num_cpus_loc);
+      SFREE(t0new);
       if(*nam>0){
-	for(i=0;i<nkn2-nkn1;i++){
+	for(i=0;i<*nk;i++){
 	  iamplitude=iamt1[konrfn[iprfn[i]]-1];
 	  for(j=1;j<iprfn[i+1]-iprfn[i];j++){
 	    if(iamt1[konrfn[iprfn[i]+j]-1]!=iamplitude){
@@ -372,21 +359,26 @@ void readnewmesh(char *jobnamec,ITG *nboun,ITG *nodeboun,ITG *iamboun,
 	    printf("        not the same.\n");
 	    FORTRAN(stop,());
 	  }
-	  iamt1[nkn1+i]=iamplitude;
+	  iamt1[i]=iamplitude;
 	}
       }
     }
 
     /* interpolating vold and veold */
 
-    for(i=0;i<nkn2-nkn1;i++){
-      vold[nkn1+i]=0.;
-      veold[nkn1+i]=0.;
+    nkmt=*nk*mt;
+    NNEW(vnew,double,nkmt);
+    NNEW(venew,double,nkmt);
+    for(i=0;i<*nk;i++){
       for(j=0;j<iprfn[i+1]-iprfn[i];j++){
-	vold[nkn1+i]+=ratiorfn[iprfn[i]+j]*vold[konrfn[iprfn[i]+j]-1];
-	veold[nkn1+i]+=ratiorfn[iprfn[i]+j]*veold[konrfn[iprfn[i]+j]-1];
+	vnew[i]+=ratiorfn[iprfn[i]+j]*vold[konrfn[iprfn[i]+j]-1];
+	venew[i]+=ratiorfn[iprfn[i]+j]*veold[konrfn[iprfn[i]+j]-1];
       }
     }
+    cpypardou(vold,vnew,&nkmt,&num_cpus_loc);
+    cpypardou(veold,venew,&nkmt,&num_cpus_loc);
+    SFREE(vnew);
+    SFREE(venew);
     
   }
 
@@ -450,12 +442,14 @@ void readnewmesh(char *jobnamec,ITG *nboun,ITG *nodeboun,ITG *iamboun,
 
     /* interpolating the initial temperatures t1 */
 
-    for(i=0;i<nkn2-nkn1;i++){
-      t1[nkn1+i]=0.;
+    NNEW(t1new,double,*nk);
+    for(i=0;i<*nk;i++){
       for(j=0;j<iprfn[i+1]-iprfn[i];j++){
-	t1[nkn1+i]+=ratiorfn[iprfn[i]+j]*t1[konrfn[iprfn[i]+j]-1];
+	t1new[i]+=ratiorfn[iprfn[i]+j]*t1[konrfn[iprfn[i]+j]-1];
       }
     }
+    cpypardou(t1,t1new,nk,&num_cpus_loc);
+    SFREE(t1new);
 
   }
 
